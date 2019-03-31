d0d11db45a6ef6d444be6b53ccf242db50c8b100
SnoviyaD <30373607+SnoviyaD@users.noreply.github.com>
2019-03-31 07:48:11 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html5>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Events</title>
+
+</head>
+
+<body >
+	<header>
+	  <h2>Daedalus BB</h2>
+	</header>
+
+	<section>
+	 	<ul>
+	  	<li><a href="home.php">Home</a></li>
+	  	<li><a class="active" href="#aboutus">About us</a></li>
+	  	<li><a href="search.php">Search</a></li>
+	  	<li><a  href="events.php">Events</a></li>
+	  	<li><a href="contactus.php">Contact us</a></li>
+	  	<li class="navwala"><a href="loginoption.php">Login</a><li>
+	  	<li class="navwala"><a  href="regoption.php">Registration</a><li>
+	  	</ul>
+  	</section>
+  	<div class="event_dis" style="padding: 15px; width: 850px;">
+  		<h4>ABOUT US</h4>
+  		<p>Daedalus is a noble charity organization formed with the active initiation and guidance of Lt. Col. (Hon.) Dr. Mohanlal, under Act For Humanity. Act For Humanity is a Non-profit organization founded by Mr. Esahaque Eswaramangalam and a group of young social workers during 2004, with Lt. Col. (Hon.) Dr. Mohanlal as its goodwill ambassador, aiming to fight against terrorism and fanatic activities. Act For Humanity aims to enhance the humanistic elements of each individual and prepare them to be tolerant, responsive, accountable and responsible social citizen.</p>
+  		<p>Daedalus  is aimed at promoting the awareness of blood donation among the public. It is committed to stay ahead of all linguistic-rational-religious-political differences and shall be fully focusing its objectives in health care activities, with technology support from WebCastle Media Pvt. Ltd., Cochin.</p>
+  		<p>
+		Daedalus  Society is registered as per The TCLSCS Registration Act XII of 1955 under the Government of Kerala. Initial operation of Indian Blood Bank shall be limited to Kerala and in the coming years its services shall be available all over India.
+  		</p>
+
+  	</div><br>
+  	<footer>
+	<p style="text-align: left; padding-left: 10px">Copyright &copy; 2019 Daedalus BB</p>
+
+	<p style=" text-align: right ;">Follow us at </p>
+	</footer>
+
+</body>
+</html>	
+	
